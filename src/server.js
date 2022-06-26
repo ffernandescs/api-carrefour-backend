ef3572045f988f0144a76ac6204b7d64855c3219
Felipe Fernandes <f.fernandescs@gmail.com>
2022-06-26 07:38:24 -0300

    Correção de CORS6

--- src/server.js
+++ src/server.js
@@ -13,36 +13,12 @@ app.get('/', async(req, res) => {
     const lojaProxima = req.query
     const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
 
-    return res.json(data)
+    return res.send(data)
 })
 
 // Ira passar informação da Cep para pagina inicial, a mesma ja recebe o valor do frontEnd
 
-app.get('/home', async(req, res) => {
-    const cepStorage = req.query
-    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cepStorage.cep}`)
-    return res.json(data)
-})
 
-// Ira passar informação da loja para pagina Lojas, a mesma ja recebe o valor do frontEnd
-
-
-app.get('/lojas', async(req, res) => {
-    const lojaProxima = req.query
-    const { data } = await axios(`https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${lojaProxima}`)
-    console.log(lojaProxima)
-
-    return res.json(data)
-})
-
-// Ira passar informação da Cep para pagina Lojas, a mesma ja recebe o valor do frontEnd
-
-
-app.get('/lojasCp', async(req, res) => {
-    const cep = req.query
-    const { data } = await axios(`https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep.cep}`)
-    return res.json(data)
-})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`)
